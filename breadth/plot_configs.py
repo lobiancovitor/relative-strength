d8eba2afa45cc251bada2e4c23ee75b31fa56f0d
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-06-23 19:36:14 -0300

    Update plot_configs.py

--- breadth/plot_configs.py
+++ breadth/plot_configs.py
@@ -22,14 +22,12 @@ def configure_plot(ax, percent_formatter=None):
 
 
 def plot_percent_above_ma(data, len):
-    
     sma_200 = data.rolling(window=200).mean()
     above_sma_200 = (data > sma_200).sum(axis=1)
     percent_above_sma_200 = (above_sma_200 / len) * 100
 
     sma_50_of_percent_above_200 = percent_above_sma_200.rolling(window=50).mean()
-    
-    
+
     def percent_formatter(x, pos):
         return f"{x:.0f}%"
 
@@ -50,10 +48,44 @@ def plot_percent_above_ma(data, len):
     ax1.set_title("IBOV Percent of Stocks Above 200 Day Moving Average (EOD)")
     ax1.legend(loc="upper left")
 
-    plt.savefig('output/$BRA200R.png', bbox_inches='tight')
+    plt.savefig("output/$BRA200R.png", bbox_inches="tight")
 
 
-def plot_mc_clellan_summation_index(summation_index_last_450, moving_average_20):
+def plot_mc_clellan_summation_index(data):
+    advances = (data.pct_change() > 0).sum(axis=1)
+    declines = (data.pct_change() < 0).sum(axis=1)
+
+    adv_dec_diff = advances - declines
+
+    # RANA normalizes the indicator by showing net advances as a percentage of advances plus declines
+
+    ana = (
+        adv_dec_diff / (advances + declines)
+    )  # StockCharts.com calculates Net Advances as a percentage of advances plus declines
+
+    ema_19_adj = ana.ewm(span=19, adjust=False).mean()
+    ema_39_adj = ana.ewm(span=39, adjust=False).mean()
+
+    adjusted_mc_clellan_oscillator = (
+        ema_19_adj - ema_39_adj
+    ) * 1000  # Obtain whole numbers and eliminate decimals
+
+    adjusted_mc_clellan_oscillator = adjusted_mc_clellan_oscillator.dropna()
+
+    summation_index = adjusted_mc_clellan_oscillator.copy()
+    summation_index.iloc[0] = adjusted_mc_clellan_oscillator.iloc[0]
+
+    for i in range(1, len(summation_index)):
+        summation_index.iloc[i] = (
+            summation_index.iloc[i - 1] + adjusted_mc_clellan_oscillator.iloc[i]
+        )
+
+    summation_index_last_450 = summation_index.iloc[-450:]
+
+    moving_average_20 = summation_index.rolling(window=20).mean()
+
+    moving_average_20 = moving_average_20.iloc[-450:]
+
     egrid = (21, 29)
     fig = mpf.figure(style="ibd", figsize=(15, 9))
     ax1 = plt.subplot2grid(egrid, (1, 0), colspan=26, rowspan=16)
@@ -66,19 +98,34 @@ def plot_mc_clellan_summation_index(summation_index_last_450, moving_average_20)
     ax1.set_title("IBOV McClellan Summation Index (Ratio Adjusted) (EOD)")
     ax1.legend(loc="upper left")
 
-    plt.savefig('output/$BRSI.png', bbox_inches='tight')
+    plt.savefig("output/$BRSI.png", bbox_inches="tight")
 
-def plot_cumulative_net_new_highs(cumulative_net_new_highs, sma_20):
+
+def plot_cumulative_net_new_highs(data):
+    window = 252
+    new_highs = data.rolling(window=window).max()
+    new_lows = data.rolling(window=window).min()
+
+    margin = 0.01
+    new_highs_52wk = data >= new_highs * (1 - margin)
+    new_lows_52wk = data <= new_lows * (1 + margin)
+
+    net_new_highs = new_highs_52wk.sum(axis=1) - new_lows_52wk.sum(axis=1)
+
+    cumulative_net_new_highs = net_new_highs.cumsum()
+
+    sma_20 = cumulative_net_new_highs.rolling(window=20).mean()
+
     egrid = (21, 29)
     fig = mpf.figure(style="ibd", figsize=(15, 9))
     ax1 = plt.subplot2grid(egrid, (1, 0), colspan=26, rowspan=16)
 
     configure_plot(ax1)
 
-    ax1.plot(cumulative_net_new_highs[-450:], label="$BRHL Cumulative", linewidth=1.5)
-    ax1.plot(sma_20[-450:], label="MA(20)", linestyle='-', linewidth=1.5)
+    ax1.plot(cumulative_net_new_highs[-900:], label="$BRHL Cumulative", linewidth=1.5)
+    ax1.plot(sma_20[-900:], label="MA(20)", linestyle="-", linewidth=1.5)
 
     ax1.set_title("IBOV Cumulative New Highs-New Lows (EOD)")
-    ax1.legend(loc='upper left')
+    ax1.legend(loc="upper left")
 
-    plt.savefig('output/$BRHL.png', bbox_inches='tight')
+    plt.savefig("output/$BRHL.png", bbox_inches="tight")
