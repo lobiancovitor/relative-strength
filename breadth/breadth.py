b8fa1f4127d7c9ab3015ff94601dd1753303e444
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-06-23 19:36:17 -0300

    Update breadth.py

--- breadth/breadth.py
+++ breadth/breadth.py
@@ -1,23 +1,11 @@
-from breadth.plot_configs import plot_percent_above_ma
-
-
-# def percent_above_ma(data):
-#     sma_200 = data.rolling(window=200).mean()
-#     above_sma_200 = (data > sma_200).sum(axis=1)
-#     percent_above_sma_200 = (above_sma_200 / len(data)) * 100
-
-#     sma_50_of_percent_above_200 = percent_above_sma_200.rolling(window=50).mean()
-
-#     plot_percent_above_ma(percent_above_sma_200, sma_50_of_percent_above_200)
-
-
-def mc_clellan_summation_index(summation_index_last_450, moving_average_20):
-    pass
-
-
-def cumulative_net_new_highs(cumulative_net_new_highs, sma_20):
-    pass
+from breadth.plot_configs import (
+    plot_percent_above_ma,
+    plot_mc_clellan_summation_index,
+    plot_cumulative_net_new_highs,
+)
 
 
 def breadth(data, len):
     plot_percent_above_ma(data, len)
+    plot_mc_clellan_summation_index(data)
+    plot_cumulative_net_new_highs(data)
