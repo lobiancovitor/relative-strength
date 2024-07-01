2e60c38b4279552632e82d4f2ac59f36dc247ad5
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-07-01 16:48:07 -0300

    Update yahoo_finance.py

--- yahoo_finance.py
+++ yahoo_finance.py
@@ -1,17 +1,22 @@
+from datetime import datetime
+
 import yfinance as yf
 
 
 def get_yf_data(securities: list) -> dict:
-    df = yf.download(securities, period="5y")
+    df = yf.download(securities, period="1y")
 
     candles = [
         {
-            "open": row["Open"],
-            "close": row["Adj Close"],
-            "low": row["Low"],
-            "high": row["High"],
-            "volume": row["Volume"],
-            "datetime": int(idx.timestamp()),
+            "open": round(float(row["Open"]), 3),
+            "close": round(float(row["Close"]), 3),
+            "low": round(float(row["Low"]), 3),
+            "high": round(float(row["High"]), 3),
+            "volume": round(float(row["Volume"]), 3),
+            "datetime": datetime.fromtimestamp(
+                int(idx.timestamp())).strftime(
+                    "%Y-%m-%d"
+                ),
         }
         for idx, row in df.iterrows()
     ]
